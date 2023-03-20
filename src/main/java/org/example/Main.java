37ed9bab3ff5cdea0ef2d25e508bc5870ad2fb8e
javadeveloperr <ergashevdilshod807@gmail.com>
2023-03-20 23:03:04 +0500

    some changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
-
         System.out.println("Hello world!");
     }
 }
